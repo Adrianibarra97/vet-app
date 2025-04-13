6d6d9d151a5a4a77c1fd829479911ad22e95f5f3
Tamara <mecozzite@gmail.com>
2025-04-13 02:27:41 -0300

    footer is added, with absolute position

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,26 @@
-export const Footer = () => {
-  return (
-    <>
-      <footer>Pié de página</footer>
-    </>
-  )
-}
+import { Typography, Box } from '@mui/material';
+
+export const Footer = () => (
+  <Box
+    component="footer"
+    sx={{
+      backgroundColor: 'var(--footer-color)',
+      py: '1em',
+      textAlign: 'center',
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+    }}
+  >
+    <Typography
+      component="p"
+      sx={{
+        fontSize: '1em',
+        fontWeight: 'bold',
+        color: 'var(--main-color)',
+      }}
+    >
+      VetApp - derechos reservados
+    </Typography>
+  </Box>
+);
