08639a1a1a164f634777ce474f23c14253dbc05c
Tamara <mecozzite@gmail.com>
2025-04-13 21:57:40 -0300

    component card was created shift
    Co-authored-by: Lucasr2003 <lgrodriguez@estudiantes.unsam.edu.ar>

---
+++ src/pages/medical-shift-page/MedicalShiftPage.tsx
@@ -0,0 +1,10 @@
+import MedicalShiftCard from "../../components/medical-shift-card/medical-shift-card"
+
+export const MedicalShiftPage = () => {
+  return (
+    <>
+    <MedicalShiftCard />
+      <h1>Turnos</h1>
+    </>
+  )
+}
