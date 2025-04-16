a3e964607872078abf7e50cbd7073f0b99ab087a
Tamara <mecozzite@gmail.com>
2025-04-16 01:32:51 -0300

    user page changes

--- src/pages/profile-page/ProfilePage.tsx
+++ src/pages/profile-page/ProfilePage.tsx
@@ -1,17 +1,44 @@
 import { ProfileMenu } from "../../components/profile-menu/Profile-menu"
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Paper } from "@mui/material";
 import ProfileForm from "../../components/user-form-component/User-form-component";
 
 export const ProfilePage = () => {
   return (
-    <Box sx={{ px: { xs: '1em', md: '4em' }, py: '2em' }}>
-    <Grid container >
-      <Grid item xs={12} md={3}>
-        <ProfileMenu />
+    <Box
+      sx={{
+        px: { xs: '1em', md: '4em' },
+        py: '2em',
+        backgroundColor: 'var(--main-color)',
+        minHeight: '100vh',
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={18} md={3}>
+          <Paper
+            elevation={3}
+            sx={{
+              backgroundColor: 'var(--primary-color)',
+              padding: '1em',
+              height: '100%',
+            }}
+          >
+            <ProfileMenu />
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} md={9}>
+          <Paper
+            elevation={8}
+            sx={{
+              backgroundColor: 'var(--header-color)',
+              padding: '2em',
+              
+            }}
+          >
+            <ProfileForm userRole="veterinarian" />
+          </Paper>
+        </Grid>
       </Grid>
-<ProfileForm userRole="veterinarian"/>
-      
-    </Grid>
-  </Box>
+    </Box>
 );
 };
