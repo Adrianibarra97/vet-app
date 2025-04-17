a503823881fcfa85afdf61ffe37baae06bd6f55c
Adrianibarra97 <adrian__ibarra@hotmail.com>
2025-04-17 11:12:19 -0300

    fix footer css

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,9 @@
-import { Typography, Box } from '@mui/material';
+import { Typography, Box } from '@mui/material'
+
+import './Footer.css'
 
 export const Footer = () => (
-  <Box
-    component="footer"
-    sx={{
-      backgroundColor: 'var(--footer-color)',
-      py: '1em',
-      textAlign: 'center',
-      position: 'absolute',
-      bottom: 0,
-      width: '100%',
-    }}
-  >
-    <Typography
-      component="p"
-      sx={{
-        fontSize: '1em',
-        fontWeight: 'bold',
-        color: 'var(--main-color)',
-      }}
-    >
-      VetApp - derechos reservados
-    </Typography>
+  <Box className='footer' component="footer">
+    <Typography className='footer__p' component="p">VetApp - derechos reservados</Typography>
   </Box>
-);
+)
