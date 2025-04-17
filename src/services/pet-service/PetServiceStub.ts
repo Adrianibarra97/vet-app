9b8bd18a331db66a728c524beae3bf20870fc4dd
Adrianibarra97 <adrian__ibarra@hotmail.com>
2025-04-17 10:26:05 -0300

    fix service

--- src/services/pet-service/PetServiceStub.ts
+++ src/services/pet-service/PetServiceStub.ts
@@ -1,7 +1,7 @@
 import { PetServiceInter } from "./PetServiceInter"
 import { Pet } from "../../domain/Pet"
 
-export class PetServiceStub implements PetServiceInter{
+export class PetServiceStub implements PetServiceInter {
   
 	async getAll(): Promise<Pet[]> {
 		return []
