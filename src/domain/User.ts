7e989218f83ff10feb0fe821c1c93769c0b666db
Adrianibarra97 <adrian__ibarra@hotmail.com>
2025-04-18 21:25:14 -0300

    domain export type, pet service and axios dependency

--- src/domain/User.ts
+++ src/domain/User.ts
@@ -1,14 +1,37 @@
+export type UserJSON = {
+	id: number,
+	dni: number,
+	name: string,
+	surname: string,
+	email: string,
+	telephone: number,
+	adress: string,
+	username: string
+}
+
 export class User {
     
-    constructor(
-        public id: number,
-        public dni: number,
-        public name: string,
-        public surname: string,
-        public email: string,
-        public telephone: number,
-        public adress: string,
-        public username: string,
-        public password: string,
-    ) {}
+	constructor(
+		public id: number,
+		public dni: number,
+		public name: string,
+		public surname: string,
+		public email: string,
+		public telephone: number,
+		public adress: string,
+		public username: string
+	) {}
+
+	toJSON(): UserJSON {
+		return {
+			id: this.id,
+			dni: this.dni,
+			name: this.name,
+			surname: this.surname,
+			email: this.email,
+			telephone: this.telephone,
+			adress: this.adress,
+			username: this.username
+		}
+	}
 }
