3027e4f6a64922488b9280f95cb2c602309f8df2
Adrianibarra97 <adrian__ibarra@hotmail.com>
2025-04-21 19:24:44 -0300

    domain export type, pet service and axios dependency

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 import './css/index.css'
+import { AxiosInterceptor } from './interceptors/axios.interceptor.tsx'
+
+AxiosInterceptor()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
