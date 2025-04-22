ead8477ddea29e803e6f5c6f5bbf1217735a2ee3
Adrianibarra97 <adrian__ibarra@hotmail.com>
2025-04-21 22:20:26 -0300

    handle error

--- src/interceptors/axios.interceptor.tsx
+++ src/interceptors/axios.interceptor.tsx
@@ -1,8 +1,31 @@
-import axios from "axios"
+import axios, { AxiosError } from 'axios'
+import { handleError } from '../util/handle-error/handleError'
 
 export const AxiosInterceptor = () => {
-  axios.interceptors.request.use((request) => {
-    console.log("Starting Request", request)
-    return request
-  })
+
+  // const updateheader = (request: AxiosRequestConfig) => {
+  //   const token = '11218683284682134682341823648123'
+  //   const newheaders = {
+  //     Authorization: token,
+  //     'Content-type': 'Application/json'
+  //   }
+  //   request.headers = newheaders
+  //   return request
+  // }
+
+  // axios.interceptors.request.use((request: AxiosRequestConfig) => {
+  //   return updateheader(request)
+  // })
+
+  axios.interceptors.response.use(
+    (response) => {
+      console.log('response', response)
+      return response
+    },
+    (error: AxiosError) => {
+      const errorCodeText: string = error.code ? error.code : 'ERROR'
+      console.log('Error:', handleError(errorCodeText))
+      return Promise.reject(error)
+    }
+  )
 }
