46fb2130c5d26a2dad2f3b497d513c4594a0bb82
Tamara <mecozzite@gmail.com>
2025-05-14 00:08:13 -0300

    photo style corrections

--- src/components/profile-menu/Profile-menu.tsx
+++ src/components/profile-menu/Profile-menu.tsx
@@ -1,12 +1,13 @@
 // src/components/profile-menu/ProfileMenu.tsx
-import {  Avatar,  Box,  IconButton,} from '@mui/material'
+import { Avatar, Box, IconButton } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import { PhotoCamera } from '@mui/icons-material'
 import { User } from '../../domain/User'
 import { ProfilePhotoModal } from '../profile-photo-modal/ProfilePhotoModal'
-import { avatarStyle, iconButtonStyle } from '../profile-photo-modal/ProfilePhotoModalStyle'
+import { iconButtonStyle } from '../profile-photo-modal/ProfilePhotoModalStyle'
 import './Profile-menu.css'
+import { avatarMenuStyle } from './ProfileMenuStyle'
 
 interface Props {
   user: User
@@ -18,29 +19,25 @@ export const ProfileMenu = ({ user, onPhotoChange }: Props) => {
   const handleOpenModal = () => setOpenModal(true)
   const handleCloseModal = () => setOpenModal(false)
 
-
   return (
     <>
       <Box sx={{ position: 'relative', width: 'fit-content' }}>
-        <Avatar
-          alt="Foto de perfil"
-          src={user.photo}
-          sx={avatarStyle}
-        />
-        <IconButton
-          onClick={handleOpenModal}
-          sx={iconButtonStyle}
-        >
+        <Avatar alt="Foto de perfil" src={user.photo} sx={avatarMenuStyle} />
+        <IconButton onClick={handleOpenModal} sx={iconButtonStyle}>
           <PhotoCamera fontSize="small" />
         </IconButton>
       </Box>
 
       <ul className="menu__ul">
-        <Link className="menu__ul--link" to="/profile">Perfil</Link>
+        <Link className="menu__ul--link" to="/profile">
+          Perfil
+        </Link>
         <Link className="menu__ul--link" to="/pets">
           {user.typeOfUser === 'petOwner' ? 'Mis mascotas' : 'Mis pacientes'}
         </Link>
-        <Link className="menu__ul--link" to="/medical-shift">Turnos</Link>
+        <Link className="menu__ul--link" to="/medical-shift">
+          Turnos
+        </Link>
       </ul>
 
       <ProfilePhotoModal
--- src/components/profile-menu/ProfileMenuStyle.ts
+++ src/components/profile-menu/ProfileMenuStyle.ts
@@ -1,7 +1,6 @@
 export const avatarMenuStyle = {
-  width: '80%',
-  height: '80%',
-
+  width: '7.5rem',
+  height: '7.5rem',
   objectFit: 'contain',
   borderRadius: '50%',
 }
