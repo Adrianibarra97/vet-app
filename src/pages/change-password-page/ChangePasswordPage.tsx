4aaa899e47633ab613b7c81099f838742eab32df
Adrianibarra97 <adrian__ibarra@hotmail.com>
2025-05-17 17:36:02 -0300

    change password page completed

--- src/pages/change-password-page/ChangePasswordPage.tsx
+++ src/pages/change-password-page/ChangePasswordPage.tsx
@@ -1,9 +1,11 @@
-import { MouseEvent, useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Button } from '@mui/material'
-import AuthServiceManager from '../../services/auth-service/AuthServiceManager'
+import { Box } from '@mui/material'
+import { FormControlModal } from '../../components/form-control-modal/FormControlModal'
+import { ButtonsModal } from '../../components/buttons-modal/ButtonsModal'
 import { SnackbarUtilities } from '../../util/snackbar/SnackbarManager'
-import { BkgButton } from './ChangePasswordPageStyle'
+import AuthServiceManager from '../../services/auth-service/AuthServiceManager'
+import { buttonContent, modalItem, modalItemText } from './ChangePasswordPageStyle'
 import './ChangePasswordPage.css'
 
 export const ChangePasswordPage = () => {
@@ -24,8 +26,7 @@ export const ChangePasswordPage = () => {
     }
   }
 
-  const handleChangePassword = (e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
-    e.preventDefault()
+  const handleChangePassword = () => {
     if(AuthServiceManager.getIntance().validNewPassword(password, confirmPassword)) {
       setErrorActive(true)
     } else {
@@ -34,21 +35,39 @@ export const ChangePasswordPage = () => {
     }
   }
 
+  const handleCancel = () => {
+    AuthServiceManager.getIntance().cancelResetPassword()
+    navigate('/auth/login')
+  }
+
   return (
-    <main className="auth__main">
-      <div className="login">
-        <h1 className="main__title">Cambiar Contraseña</h1>
-        <form className="login__form">
-          <label className={ errorActive ? "auth__text" : "auth__text--none" }>Las contraseñas no coninciden!</label>
-          {/* <FormControlModalLogin
-            isActive={ true } errorActive={ errorActive } label={ 'Nueva Contraseña' } type={ 'password' }
-            defaultValue={ '' } labelColor={ 'primary' } handleInputChanges={ (e) => setPassword(e.target.value) }
-          />
-          <FormControlModalLogin
-            isActive={ true } errorActive={ errorActive } label={ 'Confirmar Contraseña' } type={ 'password' }
-            defaultValue={ '' } labelColor={ 'primary' } handleInputChanges={ (e) => setConfirmPassword(e.target.value) }
-          /> */}
-          <Button sx={ BkgButton } onClick={ handleChangePassword }>Cambiar</Button>
+    <main className="auth__change">
+      <div className="login--change">
+        <h1 className="main__title--change">Cambiar Contraseña</h1>
+        <form className="login__form--change">
+          <Box sx={ modalItemText }>
+            <label className={ errorActive ? 'auth__text--change' : 'auth__text--none' }>Las contraseñas no coninciden!</label>
+          </Box>
+          <Box sx={ modalItem }>
+            <FormControlModal
+              isActive={ true } errorActive={ errorActive } label={ 'Nueva Contraseña' }
+              type={ 'password' } defaultValue={ '' } labelColor={ 'success' }
+              handleInputChanges={ (e) => setPassword(e.toString()) }
+            />
+          </Box>
+          <Box sx={ modalItem }>
+            <FormControlModal
+              isActive={ true } errorActive={ errorActive } label={ 'Confirmar Contraseña' }
+              type={ 'password' } defaultValue={ '' } labelColor={ 'success' }
+              handleInputChanges={ (e) => setConfirmPassword(e.toString()) }
+            />
+          </Box>
+          <Box sx={ buttonContent }>
+            <ButtonsModal
+              confirLabel={ 'Cambiar' } cancelLabel={ 'Cancelar' }
+              confirm={ () => handleChangePassword() } cancel={ () => handleCancel() }
+            />
+          </Box>
         </form>
       </div>
     </main>
--- src/pages/change-password-page/ChangePasswordPageStyle.ts
+++ src/pages/change-password-page/ChangePasswordPageStyle.ts
@@ -1,6 +1,18 @@
-export const BkgButton = {
-  width: 'auto',
-  color: 'var(--header-color)',
-  fontWeight: 'bolder',
-  backgroundColor: 'var(--footer-color)'
+export const modalItemText = {
+  height: '3em',
+  width: '100%'
+}
+
+export const modalItem = {
+  display: 'flex',
+  height: '3.5em',
+  width: '95%',
+  margin: '0.1em auto',
+  overflow: 'visible'
+}
+
+export const buttonContent = { 
+  width: '90%',
+  height: '4em',
+  margin: 0
 }
