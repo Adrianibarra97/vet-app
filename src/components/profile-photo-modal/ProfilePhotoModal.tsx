477ef46d270b797c859f5fd4ed1249037aa2be38
Adrianibarra97 <adrian__ibarra@hotmail.com>
2025-05-22 20:55:38 -0300

    use context user

--- src/components/profile-photo-modal/ProfilePhotoModal.tsx
+++ src/components/profile-photo-modal/ProfilePhotoModal.tsx
@@ -15,6 +15,7 @@ import AuthServiceManager from '../../services/auth-service/AuthServiceManager'
 import PetOwnerServiceManager from '../../services/pet-owner-service/PetOwnerServiceManager'
 import VetServiceManager from '../../services/vet-service/VetServiceManager'
 import { SnackbarUtilities } from '../../util/snackbar/SnackbarManager'
+import { useUser } from '../../context/UserContext'
 import {
   avatarStyle,
   modalStyle,
@@ -33,6 +34,8 @@ interface Props {
 }
 
 export const ProfilePhotoModal = ({ user, open, onClose, onPhotoChange }: Props) => {
+
+  const { updateUser } = useUser();
   const [selectedFile, setSelectedFile] = useState<File | null>(null)
   const [previewUrl, setPreviewUrl] = useState<string | null>(null)
   const [isUploading, setIsUploading] = useState(false)
@@ -55,6 +58,7 @@ export const ProfilePhotoModal = ({ user, open, onClose, onPhotoChange }: Props)
         Object.create(Object.getPrototypeOf(user)),
         { ...user, photo: newPhotoUrl }
       )
+      updateUser(updatedUser)
       if (AuthServiceManager.getIntance().isVet()) {
         await VetServiceManager.getInstance().update(updatedUser as Vet)
       } else {
